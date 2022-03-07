f02aae820757c2bf94743b1401da5c73a5386082
Théo Brouillé <theo.brouille@kering.com>
2022-03-07 12:05:24 +0100

    Fix localization if not using SPM

--- Sources/ChartUI/Shared/Helpers/Extensions/StringExtension.swift
+++ Sources/ChartUI/Shared/Helpers/Extensions/StringExtension.swift
@@ -1,7 +1,13 @@
 import Foundation
 
 extension String {
-    func localized(from bundle: Bundle = .module, withParameters parameters: CVarArg...) -> String {
+    func localized(withParameters parameters: CVarArg...) -> String {
+#if SWIFT_PACKAGE
+        return String(format: NSLocalizedString(self, bundle: .module, comment: ""), arguments: parameters)
+#else
+        let path = Bundle(for: ChartUI.self).path(forResource: "ChartUI", ofType: "bundle")!
+        let bundle = Bundle(path: path) ?? Bundle.main
         return String(format: NSLocalizedString(self, bundle: bundle, comment: ""), arguments: parameters)
+#endif
     }
 }
